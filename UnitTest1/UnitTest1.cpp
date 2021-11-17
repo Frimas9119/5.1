07e94f1db6966bdba262e62544361f7d8494c03e
Frimas9119 <mrhaldpvp@gmail.com>
2021-11-17 16:29:21 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:/Проекти С++/5.1/5.1.sln"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = g(a, b);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
